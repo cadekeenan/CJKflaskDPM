9f8b41254a98002d1381f05d661549cd16fd1a6b
Cade Keenan <95306859+cadekeenan@users.noreply.github.com>
2022-04-25 18:16:06 -0500

    newcalc

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -10,30 +10,20 @@ import tests.calculator as calc
 
 class TestCalculator(unittest.TestCase):  #test class
     
-    # methods
-    def test_add(self):  
-        self.assertEqual(calc.add(10, 5), 15)
-        self.assertEqual(calc.add(-1, 1), 0)  # boundary cases
-        self.assertEqual(calc.add(-1, -1), -2)
+import app
 
-    def test_subtract(self):
-        self.assertEqual(calc.subtract(10, 5), 5)
-        self.assertEqual(calc.subtract(-1, 1), -2)
-        self.assertEqual(calc.subtract(-1, -1), 0)
+def  test_calculate_top_area(): 
+    """
+    GIVEN a user enters 180 as the radius 
+    WHEN that radius is passed to this function 
+    THEN the user's top area is accurately created
+    """
+    assert app.calculate_top_area(180)== 101736
 
-    def test_multiply(self):
-        self.assertEqual(calc.multiply(10, 5), 50)
-        self.assertEqual(calc.multiply(-1, 1), -1)
-        self.assertEqual(calc.multiply(-1, -1), 1)
-    
-    def test_divide(self):
-        self.assertEqual(calc.divide(10, 5), 2)
-        self.assertEqual(calc.divide(-1, 1), -1)
-        self.assertEqual(calc.divide(-1, -1), 1)
-        self.assertEqual(calc.divide(5, 2), 2.5)
-
-        self.assertRaises(ValueError, calc.divide, 10, 0)
-    
-
-if __name__ == '__main__':
-    unittest.main()
+def  test_calculate_side_area(): 
+    """
+    GIVEN a user enters 180 as the radius and 360 as height
+    WHEN that radius and height is passed to this function 
+    THEN the user's side area is accurately created
+    """
+    assert app.calculate_side_area(180,360)== 406944
